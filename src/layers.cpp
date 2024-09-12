90a0f37c704fe8257981ae6d9673ee6b1dc4f14a
ponalex <alexponiavin@gmail.com>
2024-09-12 14:34:34 +0500

    Order of operations in backword method was changed according to previous commit. Bug fix in << operator.

--- src/layers.cpp
+++ src/layers.cpp
@@ -10,12 +10,12 @@ FCNetwork<VSize>::FCNetwork(arma::Mat<VSize> initalized_weights,
 }
 
 template <class VSize> std::ostream& operator<<(std::ostream& stream, const FCNetwork<VSize>& layer){
-    stream << "Neuron numbers: " << layer.weights.n_rows << "\n";
-    stream << "Input numbers: " << layer.weights.n_cols << "\n";
+    stream << "Neuron numbers: " << layer.weights.n_cols<< "\n";
+    stream << "Input numbers: " << layer.weights.n_rows<< "\n";
     stream << "Weights:\n" << layer.weights;
     stream << "Dot product:\n" << layer.dot_prod;
     stream << "Output:\n" << layer.output;
-    return stream;;
+    return stream;
 }
 
 template <class VSize> 
@@ -51,7 +51,7 @@ arma::Mat<VSize> FCNetwork<VSize>::backword(arma::Mat<VSize>& error){
         default:;
     }
     result %=  error;
-    result =  weights.t() * result ;
+    result =  result * weights.t() ;
     return result;
 }
 
@@ -73,7 +73,7 @@ void FCNetwork<VSize>::train(
             break;
         default:break;
     }
-    grad = -1.0 * grad * input.t();
+    grad = -1.0 * input.t() * grad ;
     trainer.change_weights(grad, weights);
 }
 
